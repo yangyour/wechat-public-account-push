eb7c99a16001d365f51b6513015431c158b2292d
yangyour <48142985+yangyour@users.noreply.github.com>
2024-06-25 16:29:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'owW5N6yWLRkYTlpFHk7HawYzuoKM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	_ddSfnw1MylYqBHiHh1wqjnKKI3c7CSmen2iHkUp1Bo',
+      useTemplateId: '_ddSfnw1MylYqBHiHh1wqjnKKI3c7CSmen2iHkUp1Bo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-23',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	_ddSfnw1MylYqBHiHh1wqjnKKI3c7CSmen2iHkUp1Bo',
+  CALLBACK_TEMPLATE_ID: '_ddSfnw1MylYqBHiHh1wqjnKKI3c7CSmen2iHkUp1Bo',
 
   CALLBACK_USERS: [
     {
